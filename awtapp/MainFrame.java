da2e3e7485d235f4a34721f1fca756d3c272009d
LesbatHaithem <168595728+LesbatHaithem@users.noreply.github.com>
2025-10-15 14:12:26 +0100

    lambda & anonymous Class usecase

--- awtapp/MainFrame.java
+++ awtapp/MainFrame.java
@@ -1,11 +1,15 @@
 package awtapp ;
-import java.awt.Frame ;
-import java.awt.Panel ;
-import java.awt.Button ;
-import java.awt.event.* ;
+import java.awt.* ;
+// import java.awt.Panel ;
+// import java.awt.Button ;
+// import java.awt.event.* ;
+// import java.awt.Label;
+// import java.awt.TextField;
+
 
 
 public class MainFrame extends Frame  {
+    Label lbl ;
 
     public MainFrame (){
         super() ;
@@ -18,16 +22,27 @@ public class MainFrame extends Frame  {
         Panel p = new Panel() ; // this has been replaced by Button b
         this .add(p) ;
         this .setSize(400, 300) ;
+        lbl = new Label("Hello AWT") ;
+        p .add(lbl) ;  
 
 
         Button b = new Button("Click Me") ;
         p .add(b) ;
-        b .addActionListener(new MyActionListener()) ;
+
+        // b .addActionListener(new MyActionListener()) ;
+        
+        // lambda expression to handle button click event and optimize the code
+        b.addActionListener((e)->{ lbl.setText("Button Clicked");}) ;
+
+
         Button c = new Button("Cancel") ;
         p .add(c) ;    //another action listener for cancel button
+
+        // another way to add action listener without implementing the interface with using anonymous class
         c .addActionListener(new java.awt.event.ActionListener() {
             @Override
             public void actionPerformed(java.awt.event.ActionEvent e) {
+                lbl.setText("Cancel Button Clicked");
                 System.out.println("Cancel Button Clicked");
                 e.getSource();
             }
@@ -38,7 +53,7 @@ public class MainFrame extends Frame  {
         // sizButton .setBounds(50, 100, 80, 30) ; setBounds is used to set the position and size of the button
         // this .add(sizButton) ;
 
-       
+        
         //this .setExtendedState(MAXIMIZED_BOTH); // this is for full screen
         this .setVisible(true) ;
         // this.addButtonListener(new java.awt.event.ActionListener() {
@@ -46,6 +61,10 @@ public class MainFrame extends Frame  {
         //         System.out.println("Button Clicked");
         //     }
         // });
+
+
+
+        
         // make the window exit
         this .addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowClosing(java.awt.event.WindowEvent e) {
--- awtapp/MyActionListener.java
+++ awtapp/MyActionListener.java
@@ -1,10 +1,22 @@
-package awtapp;
+// seperate file for action listener is replaced by lambda expression in MainFrame.java or anonymous class
 
-public class MyActionListener implements java.awt.event.ActionListener {
-    @Override
-    public void actionPerformed(java.awt.event.ActionEvent e) {
-        System.out.println("Button Clicked");
-        e.getSource();
-    }
+// package awtapp;
+// import java.awt.event.ActionListener;
+
+// import java.awt.Component;
+
+// public class MyActionListener implements ActionListener {
+//     @Override
+//     public void actionPerformed(java.awt.event.ActionEvent e) {
+
+//       java.awt.Component s = (Component)e.getSource();
+//       s.getParent();
+//       if (s.getName() =="b"){
+//             System.out.println("Button Clicked");
+//       }
+//       else if (s.getName() =="c"){
+//             System.out.println("Cancel Button Clicked");
+//       }
     
-}
+// }
+// }
